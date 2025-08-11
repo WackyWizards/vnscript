927a00146678f0a868d4d564a98203e39a5d3c01
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-11 21:58:08 +0300

    Prettier formatting in parsing.ts

--- server/src/parsing.ts
+++ server/src/parsing.ts
@@ -1,43 +1,48 @@
-import {
-	Diagnostic,
-	DiagnosticSeverity,
-} from 'vscode-languageserver/node';
+import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver/node';
 
-import { 
-	validateContent, 
-	addDiagnostic 
-} from "./validation";
+import { validateContent, addDiagnostic } from './validation';
 
-import { TextDocument } from "vscode-languageserver-textdocument";
+import { TextDocument } from 'vscode-languageserver-textdocument';
 
-export function parseText(text: string, textDocument: TextDocument, diagnostics: Diagnostic[]) {
-    const length = text.length;
-    let i = 0;
+export function parseText(
+  text: string,
+  textDocument: TextDocument,
+  diagnostics: Diagnostic[]
+) {
+  const length = text.length;
+  let i = 0;
 
-    while (i < length) {
+  while (i < length) {
+    if (text[i] === '(') {
+      const start = i;
+      i++;
+      let depth = 1;
+
+      while (i < length && depth > 0) {
         if (text[i] === '(') {
-            const start = i;
-            i++;
-            let depth = 1;
-
-            while (i < length && depth > 0) {
-                if (text[i] === '(') {
-                    depth++;
-                } else if (text[i] === ')') {
-                    depth--;
-                }
-                i++;
-            }
-
-            if (depth === 0) {
-                const content = text.substring(start + 1, i - 1).trim();
-                validateContent(content, start, i, textDocument, diagnostics);
-            } else {
-                addDiagnostic(diagnostics, DiagnosticSeverity.Error, textDocument, { index: start, length: length - start, input: text }, `Unclosed parenthesis.`);
-                break;
-            }
-        } else {
-            i++;
+          depth++;
+        } else if (text[i] === ')') {
+          depth--;
         }
+        i++;
+      }
+
+      if (depth === 0) {
+        const content = text.substring(start + 1, i - 1).trim();
+        validateContent(content, start, i, textDocument, diagnostics);
+      } else {
+        addDiagnostic(
+          diagnostics,
+          DiagnosticSeverity.Error,
+          textDocument,
+          start,
+          length - start,
+          'Unclosed parenthesis.'
+        );
+        break;
+      }
+    } else {
+      i++;
     }
+  }
 }
