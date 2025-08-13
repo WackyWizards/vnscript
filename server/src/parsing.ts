82f9c69b15c58744ddc642b47e084ad6d7b5c91e
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-13 18:10:53 +0300

    parsing.ts calls validateScript

--- server/src/parsing.ts
+++ server/src/parsing.ts
@@ -1,48 +1,11 @@
-import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver/node';
-
-import { validateContent, addDiagnostic } from './validation';
-
+import { Diagnostic } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
+import { validateScript } from './validation';
 
 export function parseText(
   text: string,
   textDocument: TextDocument,
   diagnostics: Diagnostic[]
 ) {
-  const length = text.length;
-  let i = 0;
-
-  while (i < length) {
-    if (text[i] === '(') {
-      const start = i;
-      i++;
-      let depth = 1;
-
-      while (i < length && depth > 0) {
-        if (text[i] === '(') {
-          depth++;
-        } else if (text[i] === ')') {
-          depth--;
-        }
-        i++;
-      }
-
-      if (depth === 0) {
-        const content = text.substring(start + 1, i - 1).trim();
-        validateContent(content, start, i, textDocument, diagnostics);
-      } else {
-        addDiagnostic(
-          diagnostics,
-          DiagnosticSeverity.Error,
-          textDocument,
-          start,
-          length - start,
-          'Unclosed parenthesis.'
-        );
-        break;
-      }
-    } else {
-      i++;
-    }
-  }
+  validateScript(text, textDocument, diagnostics);
 }
